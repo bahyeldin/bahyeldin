e6c69282cc51e8cd84d37ae6123415a025fb9ebe
bahyeldin <53016851+bahyeldin@users.noreply.github.com>
2024-07-13 19:31:28 +0300

    Add files via upload

---
+++ stock_no_negative/__manifest__.py
@@ -0,0 +1,14 @@
+
+{
+    'name': 'Stock No Negative',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'summary': 'Prevent stock moves if there is not enough stock',
+    'depends': ['stock'],
+    'author': 'Bahyeldin',
+    'description': 'Prevent stock moves if there is not enough stock',
+    'data': [],
+    'images': ['static/description/icon.png', 'static/description/thumbnail.png'],
+    'installable': True,
+    'auto_install': False,
+}
