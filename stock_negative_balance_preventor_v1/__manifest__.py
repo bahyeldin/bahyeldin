1cad3f6eb7677264267b218eca4bc343c1be7608
bahyeldin <53016851+bahyeldin@users.noreply.github.com>
2024-07-13 21:19:56 +0300

    Stock Negative Balance Preventor
    Stock Negative Balance Preventor

---
+++ stock_negative_balance_preventor_v1/__manifest__.py
@@ -0,0 +1,26 @@
+
+{
+    'name': 'Stock Negative Balance Preventor V1.0',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'price': '10',
+    'currency': 'USD',
+    'summary': 'Prevent stock moves if there is not enough stock',
+    'description': '''
+        This module prevents stock moves from being confirmed if there is not enough stock available.
+        Features
+        
+            Checks stock availability before confirming stock moves
+            Prevents negative stock levels
+            Displays a warning message if stock is insufficient
+        
+        
+       Bahyeldin Abdelhady
+    ''',
+    'author': 'Bahyeldin Abdelhady',
+    'depends': ['stock'],
+    'data': [],
+    'images': ['static/description/icon.png', 'static/description/thumbnail.png', 'static/description/image.png'],
+    'installable': True,
+    'auto_install': False,
+}
